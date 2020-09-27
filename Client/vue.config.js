2e8d304cc11763dc9fde3f7725d34dac688844fb
Ryan Blaikie <ryan.blaikie@gmail.com>
2020-09-27 22:56:31 +1300

    Procfile Server/index.js

--- Client/vue.config.js
+++ Client/vue.config.js
@@ -4,15 +4,15 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ], 
-  outputDir: path.resolve(__dirname, '../Server/public'),
-  devServer :{
-    proxy : {
-      '/saveData' : {
-        target: 'http://localhost:5000'
-      }, 
-      '/getData' : {
-        target : 'http://localhost:5000'
-      }
-    }
-  }
+  // outputDir: path.resolve(__dirname, '../Server/public'),
+  // devServer :{
+  //   proxy : {
+  //     '/saveData' : {
+  //       target: 'http://localhost:5000'
+  //     }, 
+  //     '/getData' : {
+  //       target : 'http://localhost:5000'
+  //     }
+  //   }
+  // }
 }
